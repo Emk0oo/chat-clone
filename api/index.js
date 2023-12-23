2b3647210372b0d8683804862d424a79467eec2f
Emk0oo <emir.j2002@gmail.com>
2023-12-24 00:56:47 +0100

    [ADD] user schema mongo, jwt

--- api/index.js
+++ api/index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const url= "localhost:4000/test";
-console.log('Serveur node en ligne! ', url);
-// rest of your code
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
 
-const app= express();
+const url = "localhost:4000/test";
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
-app.get('/test', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Ca marche'
-    });
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.json({
+    ok: true,
+    msg: "Ca marche",
+  });
 });
 
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const createdUser = await User.create({ username, password });
+  jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie("token", token).status(201).json({ ok });
+  });
+});
+console.log("Serveur node en ligne! ", url);
+// emirchatadmin
+// 5jc7LPBQ9Mfdo9OJ
 app.listen(4000);
